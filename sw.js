278c674a741bb8b2fc5433901e496c1ac070053f
epicjernigaming <epicjernigaming@gmail.com>
2026-04-18 22:08:42 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'digital-fob-v3';   // ← changed from v2 or v1 to v3
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(v3).then(cache => {
+    caches.open(v4).then(cache => {
       return cache.addAll([
         '/index.html',
         '/favicon.png',

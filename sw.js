d6129e6da5aeb5c2829c1513ab274cbfbdd4c3ca
epicjernigaming <epicjernigaming@gmail.com>
2026-04-19 13:00:23 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,45 +2,21 @@ const CACHE_NAME = 'digital-fob-cache-v2';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/background.jpg',
-  '/favicon.png',
   '/manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=VT323&display=swap'
+  '/background.jpg',
+  '/favicon.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
-      })
-  );
-});
-
-// Update cache when new version is activated
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then(response => response || fetch(event.request))
   );
 });
